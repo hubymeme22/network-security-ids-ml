8b82141e3513394332a598025dcf751a9406b665
hubymeme22 <huby.new1112@gmail.com>
2026-07-01 19:01:41 +0800

    initialize alembic migration

---
+++ src/backend/alembic/versions/0ded75604596_create_users_table.py
@@ -0,0 +1,39 @@
+"""create users table
+
+Revision ID: 0ded75604596
+Revises: 
+Create Date: 2026-07-01 18:54:49.066174
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "0ded75604596"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table("users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("username", sa.String(), nullable=False),
+        sa.Column("hashed_password", sa.String(), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=True),
+        sa.PrimaryKeyConstraint("id")
+    )
+
+    op.create_index(op.f("ix_users_id"), "users", ["id"], unique=False)
+    op.create_index(op.f("ix_users_username"), "users", ["username"], unique=True)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(op.f("ix_users_username"), table_name="users")
+    op.drop_index(op.f("ix_users_id"), table_name="users")
+    op.drop_table("users")
